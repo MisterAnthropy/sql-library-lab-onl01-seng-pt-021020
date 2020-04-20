24b329889bbf2d478ec484b311a6c7f1941428bf
Tyler Croley <tjcroley@outlook.com>
2020-04-20 18:07:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,27 +1,14 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("A Song of Ice and Fire", 1, 1);
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Second Series", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
-INSERT INTO Books (title, year, series_id) VALUES ("Game of Thrones", 1996, 1);
-INSERT INTO Books (title, year, series_id) VALUES ("A Clash of Kings", 1998, 1);
-INSERT INTO Books (title, year, series_id) VALUES ("A Storm of Swords", 2000, 1);
-INSERT INTO Books (title, year, series_id) VALUES ("First Book", 2002, 2);
-INSERT INTO Books (title, year, series_id) VALUES ("Second Book", 2003, 2);
-INSERT INTO Books (title, year, series_id) VALUES ("Third Book", 2005, 2);
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Lady", "Woof Woof", "direwolf", 1);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Tyrion Lannister", "A Lannister always pays his debts", "human", 1);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Daenerys Targaryen", "If I look back I am lost", "human", 1);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES (4, "Eddard Stark", "Winter is coming", "human", 1);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Character One", "motto one", "cylon", 2);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Character Two", "motto two", "human", 2);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Character Three", "motto three", "cylon", 2);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Character Four", "motto four", "cylon", 2);
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
-INSERT INTO SubGenres (name) VALUES ("medieval");
-INSERT INTO SubGenres (name) VALUES ("space opera");
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-INSERT INTO Authors (name) VALUES ("George R. R. Martin");
-INSERT INTO Authors (name) VALUES ("Second Author");
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
 
 INSERT INTO character_books (book_id, character_id) VALUES (1, 1);
 INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
